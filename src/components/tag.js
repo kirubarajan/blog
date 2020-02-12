4a567ba56c19f966ec7291944d42a424b9024f0e
Arun Kirubarajan <kiruba@cis.upenn.edu>
2020-02-11 19:56:54 -0500

    Fix tag colours

--- src/components/tag.js
+++ src/components/tag.js
@@ -7,9 +7,8 @@ function getColour(name) {
   if (name == "personal") return "is-light is-danger";
   if (name == "natural language processing") return "is-light is-success";
   if (name == "notes") return "is-light is-warning";
-  if (name == "programming") return "is-primary";
+  if (name == "programming") return "is-light is-primary";
   if (name == "research") return "is-light is-primary";
-  if (name == "list") return "is-link is-light";
 }
 
 const Tag = (props) => {
