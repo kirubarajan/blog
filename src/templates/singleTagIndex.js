b06a3f492e78799c46979048603f8dbdd8f8513e
Arun Kirubarajan <kiruba@cis.upenn.edu>
2020-02-12 15:42:13 -0500

    Update tag styles

--- src/templates/singleTagIndex.js
+++ src/templates/singleTagIndex.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from 'gatsby';
+import '../styles/styles.sass'
 
 const SingleTagTemplate = ({data, pageContext}) => {
   const { posts, tagName } = pageContext;
