0bb15568464fe7e264a4cf0cab8fc6472959d76c
Arun Kirubarajan <kiruba@cis.upenn.edu>
2020-02-11 20:44:25 -0500

    Fix 192 link

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ const Index = () => {
 	          <h1 className="title"> Hi! I'm Arun. 🙌 </h1>
 	          <p>
 	            I'm a Canadian (🇨🇦) undergraduate at the University of Pennsylvania studying computer science. 
-              I research machine learning in Penn's <a href="https://penn-nlp.github.io/">NLP Group</a> and I also <a href="https://www.cis.upenn.edu/~cis192/arun/">teach</a> one of the computer science courses at Penn.
+              I research machine learning in Penn's <a href="https://penn-nlp.github.io/">NLP Group</a> and I also <a href="https://cis192.github.io/">teach</a> one of the computer science courses at Penn.
               I'm currently a partner at <a href="https://dormroomfund.com">Dorm Room Fund</a> and an incoming intern at <a href="https://bridgewater.com"> Bridgewater Associates</a>.
 
 	            
