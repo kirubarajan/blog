8064a66832025187fcf56dbe1d998aa8fb440abd
Arun Kirubarajan <kiruba@cis.upenn.edu>
2020-05-12 11:46:59 -0400

    Update research

--- src/pages/research.js
+++ src/pages/research.js
@@ -19,15 +19,15 @@ const Research = () => {
 	            <img src={"/img/thinking.svg"}  />
 	          </figure>
 			  <h1 class="title"> my research</h1>
-              <p><strong>Papers</strong></p>
+              <p><strong>Published Papers</strong></p>
               <div class="panel list-group" style={{marginTop: "1rem", borderRadius: "0.2rem"}}>
                 <a class="panel-block list-group-item is-primary" href="https://www.aclweb.org/anthology/N19-4011/">ChatEval: A Tool for Chatbot Evaluation [NAACL 2019]</a>
                 <a class="panel-block list-group-item is-primary" href="https://spie.org/conferences-and-exhibitions/past-conferences-and-exhibitions/defense--commercial-sensing-2016">Cloud4ISR: Large-scale intelligence, surveillance, and reconnaissance [SPIE 2016]</a>
               </div>
               <p><strong>Projects</strong></p>
               <div class="panel list-group" style={{marginTop: "1rem", borderRadius: "0.2rem"}}>
-				<a class="panel-block list-group-item is-primary" href="https://github.com/kirubarajan/trick">Learning to Trick Humans: Evaluation Criteria for Human and Computer Written Text</a>
-				<a class="panel-block list-group-item is-primary" href="https://github.com/kirubarajan/trick">Learning Temporal Event Reasoning Using Wiki-How</a>
+				<a class="panel-block list-group-item is-primary" href="https://kirubarajan.nyc3.digitaloceanspaces.com/learning_to_trick_humans.pdf">Learning to Trick Humans: Evaluation Criteria for Human and Computer Written Text</a>
+				<a class="panel-block list-group-item is-primary" href="https://kirubarajan.nyc3.digitaloceanspaces.com/kirubarajan_kashyap_zhang.pdf">Fine-Tuning BERT on Wiki-How for Temporal Event Reasoning</a>
 				<a class="panel-block list-group-item is-primary" href="https://kirubarajan.nyc3.digitaloceanspaces.com/ESE546_fp_learning_game_of_life.pdf">Deep DNA (CRISPR) Lineage Tree Reconstruction</a>
 				<a class="panel-block list-group-item is-primary" href="https://kirubarajan.nyc3.digitaloceanspaces.com/530_Final_Project_1_.pdf">GROVER: Generating Rap by Observing Verses</a>
               </div>
