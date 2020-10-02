45498a0fffd5674eb130d469e2ac3601a17acbd7
Arun Kirubarajan <kiruba@cis.upenn.edu>
2020-10-02 15:40:15 -0400

    Update site

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,13 +18,13 @@ const Index = () => {
 	          <figure className="image is-128x128">
 	            <img src={"/img/hello.svg"}  />
 	          </figure>
-	          <h1 className="title"> Hi! I'm Arun. 🙌 </h1>
+	          <h1 className="title"> Hi, I'm Arun! 🙌 </h1>
 	          <p>
-	            I'm an undergraduate at the University of Pennsylvania studying computer science and minoring in linguistics. 
-              I'm a <a href="/research">published</a> machine learning researcher in Penn's <a href="https://penn-nlp.github.io/">NLP Group</a> and I also <a href="https://cis192.github.io/">teach a course</a> in Penn's computer science department!
+	            I'm a Canadian 🇨🇦 undergraduate at the University of Pennsylvania studying computer science and minoring in linguistics. 
+              I'm a <a href="/research">published</a> machine learning researcher in Penn's <a href="https://penn-nlp.github.io/">NLP Group</a> and I also <a href="https://cis192.github.io/">teach</a> in Penn's official Python course.
               
               <br /> <br />
-              	Outside of school, I'm currently a Managing Partner at a venture capital firm named <a href="https://dormroomfund.com">Dorm Room Fund</a> and a Quantitative Researcher at an AI company named <a href="https://vise.com">Vise</a>.
+              	Outside of school, I'm currently a Managing Partner at a venture capital firm named <a href="https://dormroomfund.com">Dorm Room Fund</a> and a Quantitative Researcher at an AI-powered wealth management company named <a href="https://vise.com">Vise</a>.
 			  	Previously I was an intern at a hedge fund named <a href="https://bridgewater.com"> Bridgewater</a>.
 			   	I also do consulting and contract work with a variety of organizations, ranging from Y&#8209;Combinator backed startups to R&D/defense companies.
 
---
+++ src/pages/sapir-whorf/index.md
@@ -0,0 +1,13 @@
+---
+path: "/blog/sapir-whorf"
+date: "2020-08-02"
+title: "Linguistic Relativity and Programming Languages"
+tags: ['programming']
+excerpt: "Do programming languages change how you think?"
+---
+
+# Linguistic Relativity and Programming Languages
+> Do programming languages change how you think?
+
+## Languages are Languages
+ruminating....
