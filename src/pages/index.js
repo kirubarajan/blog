ab1c0173b586f5ba9bd5b14d86e77ef7659647f4
Arun Kirubarajan <kiruba@cis.upenn.edu>
2020-11-13 18:04:48 -0500

    Update wording

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,12 +21,12 @@ const Index = () => {
 	          <h1 className="title"> Hi, I'm Arun! 🙌 </h1>
 	          <p>
 	            I'm a (Canadian! 🇨🇦) undergraduate at the University of Pennsylvania studying computer science and minoring in linguistics. 
-              I'm a <a href="/research">published</a> machine learning researcher in Penn's <a href="https://penn-nlp.github.io/">NLP Group</a> and I also <a href="https://cis192.github.io/">teach</a> Penn's official Python course!
+              I'm also a <a href="/research">published</a> machine learning researcher in Penn's <a href="https://penn-nlp.github.io/">NLP Group</a> and I teach Penn's official <a href="https://cis192.github.io/"> Python course</a>.
               
               <br /> <br />
               	Outside of school, I'm currently a Managing Partner at a venture capital firm named <a href="https://dormroomfund.com">Dorm Room Fund</a>.
-			  	Previously, I was an intern at an AI-powered wealth management startup named <a href="https://vise.com">Vise</a> and at a hedge fund named <a href="https://bridgewater.com"> Bridgewater</a>.
-			   	I also do consulting and contract work with a variety of organizations, ranging from Y&#8209;Combinator backed startups to R&D/defense companies.
+			  	Previously, I've worked at a couple of places, such as <a href="https://foursquare.com">Foursquare</a>, <a href="https://vise.com">Vise</a>, and <a href="https://bridgewater.com">Bridgewater</a>.
+			   	I also consult with a variety of organizations, ranging from Y&#8209;Combinator backed startups to R&D/defense companies.
 
               <br /> <br />
               Other than that, I like brewing coffee, skateboarding, and birdwatching! 
