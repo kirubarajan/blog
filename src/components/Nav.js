41f6f6907f1908807c9357be8aa9f8df375c8623
Arun Kirubarajan <kiruba@cis.upenn.edu>
2021-04-25 17:45:08 -0400

    Remove consulting

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,10 +6,9 @@ const Nav = () => {
   return (
     <div style={{marginTop: "1rem", marginBottom: "2rem"}}>
       <nav>
-        <a href="/"> <span className="tag is-light"> Home </span> </a>
+        <a href="/"> <span className="tag is-light"> About </span> </a>
         <a href="/research"> <span className="tag is-light"> Research </span> </a>
         <a href="/engineering"> <span className="tag is-light"> Engineering </span> </a>
-        <a href="/consulting"> <span className="tag is-light"> Consulting </span> </a>
         <a href="/teaching"> <span className="tag is-light"> Teaching </span> </a>
         <a href="/blog"> <span style={{marginTop: "0.3rem"}} className="tag is-light"> Blog </span> </a>
       </nav>
