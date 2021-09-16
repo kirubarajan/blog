c52af7d56a8ba97fba83ad6e2783c07846e321b2
Arun Kirubarajan <kiruba@cis.upenn.edu>
2021-09-16 18:58:17 -0400

    Update nav spacing

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,11 +6,11 @@ const Nav = () => {
   return (
     <div style={{marginTop: "1rem", marginBottom: "2rem"}}>
       <nav>
-        <a href="/"> <span className="tag is-light mr-1"> About </span> </a>
-        <a href="/research"> <span className="tag is-light mr-1"> Research </span> </a>
-        <a href="/engineering"> <span className="tag is-light mr-1"> Engineering </span> </a>
-        <a href="/teaching"> <span className="tag is-light mr-1"> Teaching </span> </a>
-        <a href="/blog"> <span style={{marginTop: "0.3rem"}} className="tag is-light mr-1"> Blog </span> </a>
+        <a href="/"> <span className="tag is-light"> About </span> </a>
+        <a href="/research"> <span className="tag is-light"> Research </span> </a>
+        <a href="/engineering"> <span className="tag is-light"> Engineering </span> </a>
+        <a href="/teaching"> <span className="tag is-light"> Teaching </span> </a>
+        <a href="/blog"> <span style={{marginTop: "0.3rem"}} className="tag is-light"> Blog </span> </a>
       </nav>
     </div>
   )
